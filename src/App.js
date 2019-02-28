dadffd7e7dba573dcd37500a45c8f739ef47faf5
Anuj Sharma <anujshama4u@gmail.com>
2019-02-28 22:07:02 +0530

    Added travis config

--- src/App.js
+++ src/App.js
@@ -11,11 +11,18 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>
+            React JS integration with Travis CI and Surge by &nbsp; 
+            <a className="App-link"
+            href="https://anujsh.com"
+            target="_blank">
+              Anuj Sharma
+            </a>
+          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
-            rel="noopener noreferrer"
           >
             Learn React
           </a>
